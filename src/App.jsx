ded387367e9ca61a2e4c7432ccc1fe5cf205f5d8
Shealyn Sugai <shealyn@student.dev-island.org>
2024-02-22 18:45:33 -1000

    setup overall layout and basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>React Contacts</h1>
+      <div className="contact-card">
+        <h3>Jenny Smith</h3>
+        <p>Phone: 808-867-5309</p>
+        <p>Email: jenny@gmail.com</p>
+      </div>
+    </>
+  );
 }
 
 export default App;
