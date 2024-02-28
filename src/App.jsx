6aed32e12617a7ae407c86b7275653a9b47ffe0f
Shealyn Sugai <shealyn@student.dev-island.org>
2024-02-27 19:32:08 -1000

    setup contact form structure and state

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,91 @@ import { useState } from "react";
 import ContactList from "./components/ContactList/ContactList";
 import "./App.css";
 import dummyContacts from "./contacts";
+import MaskedInput from "react-text-mask";
 
 function App() {
   const [contacts, setContacts] = useState(dummyContacts);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <>
-      <h1>React Contacts</h1>
-      <ContactList contacts={contacts} />
+      <div className="page">
+        <h1>React Contacts</h1>
+
+        <form>
+          <label>
+            First Name:
+            <input
+              type="text"
+              onChange={(event) => {
+                const value = event.target.value;
+                setFirstName(value);
+              }}
+            />
+          </label>
+
+          <label>
+            Last Name:
+            <input
+              type="text"
+              onChange={(event) => {
+                const value = event.target.value;
+                setLastName(value);
+              }}
+            />
+          </label>
+
+          <label>
+            Phone Number:{" "}
+            <MaskedInput
+              mask={[
+                /\d/,
+                /\d/,
+                /\d/,
+                "-",
+                /\d/,
+                /\d/,
+                /\d/,
+                "-",
+                /\d/,
+                /\d/,
+                /\d/,
+                /\d/,
+              ]}
+              guide={true}
+              onChange={(event) => {
+                const value = event.target.value;
+                setPhoneNumber(value);
+              }}
+            />
+          </label>
+
+          <label>
+            Email Address:{" "}
+            <input
+              type="email"
+              pattern=".+@example\.com"
+              onChange={(event) => {
+                const value = event.target.value;
+                setEmail(value);
+              }}
+            ></input>
+            <button
+              type="submit"
+              onClick={(event) => {
+                event.preventDefault();
+                console.log("Submit clicked!");
+              }}
+            >
+              Create Contact
+            </button>
+          </label>
+        </form>
+        <ContactList contacts={contacts} />
+      </div>
     </>
   );
 }
